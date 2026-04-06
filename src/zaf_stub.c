27958313eaf915f57dbb642fb271363fa19a6b30
skiselev <eve.kodi@gmail.com>
2026-04-06 22:57:47 +0300

    modified:   src/zaf_stub.c

--- src/zaf_stub.c
+++ src/zaf_stub.c
@@ -1,14 +1,13 @@
 #include <zephyr/sys/util.h>
-#include <zmk/adaptive_feedback.h>
 
 #if !IS_ENABLED(CONFIG_ZMK_ADAPTIVE_FEEDBACK)
 
-void zaf_error_trigger(int id)
+__attribute__((weak)) void zaf_error_trigger(int id)
 {
     ARG_UNUSED(id);
 }
 
-void zaf_error_clear(int id)
+__attribute__((weak)) void zaf_error_clear(int id)
 {
     ARG_UNUSED(id);
 }
